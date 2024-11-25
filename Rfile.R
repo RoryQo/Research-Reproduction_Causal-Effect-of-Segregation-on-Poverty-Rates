9bdd1a9896f59e6b073876fa60133f44b1b7da9d
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2024-11-25 18:19:26 -0500

    Update Rfile.R

---
+++ Rfile.R
@@ -0,0 +1,13 @@
+## ----setup, include=FALSE------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ----library, message=F,warning=F----------------------------------
+library(tinytex)
+library(dplyr)
+library(haven)
+library(stargazer)
+library(ggplot2)
+library(lfe)
+library(kableExtra)
+library(RColorBrewer)
